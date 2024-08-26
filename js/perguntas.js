474a8100b997c31f05884f6772e4966a53a20cfa
gustavo de paula zampieri <128715598+NINJAPRECOCE@users.noreply.github.com>
2024-08-26 09:30:52 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+const perguntas = [
+    {
+        enunciado: "você conhece o esporte mais praticado do mundo?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "daora",
+                    "que bom que sabe"
+                    ]
+            },
+            {
+                texto: "não",
+                afirmacao: [
+                    "muito burro",
+                    "lhe falta conhecimento"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "playstation ou xbox ?",
+        alternativas: [
+            {
+                texto:"playstation",
+                afirmacao: [
+                    "bom gosto",
+                    "melhor console"
+                    ]
+            },
+            {
+                texto: "xbox",
+                afirmacao: [
+                    "renasce na vida",
+                    "pessimo gosto"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "futebol ou volei",
+        alternativas: [
+            {
+                texto:"futebol",
+                afirmacao: [
+                    "muito bom",
+                    "daora"
+                    ]
+            },
+            {
+                texto:"volei",
+                afirmacao: [
+                    "daora tambem",
+                    "mas futebol é melhor"
+                    ]
+            }
+            
+        ]
+    },
+];
