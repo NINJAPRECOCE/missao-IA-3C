374f3b1ae6bbc271958b6a2cc454a7262fb41d16
gustavo de paula zampieri <128715598+NINJAPRECOCE@users.noreply.github.com>
2024-09-18 07:46:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 import {aleatorio, nome} from ' ./aleatorio.js';
 import {perguntas} from ' ./perguntas.js';
 
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-
+const botaoJogarNOvamente = document.querySelector (".novamente-b
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
 
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {
+    atual = 0;
+    historiaFinal = " ";
+    telaInicial.style.display = none;
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostar");
+    caixaResultado.classList.remove("mostar");
+    mostraPergunta();
+}
+    
 function mostraPergunta() {
     if(atual >= perguntas.length){
         mostraResultado();
@@ -48,4 +60,4 @@ function mostraResultado(){
 }
 
 
-mostraPergunta();
+SubstituiNone();
