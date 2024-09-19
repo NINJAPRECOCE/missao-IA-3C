5de241fc9e5ee918df8765f2b80a79191c65d039
gustavo de paula zampieri <128715598+NINJAPRECOCE@users.noreply.github.com>
2024-09-19 07:40:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-import {aleatorio, nome} from ' ./aleatorio.js';
-import {perguntas} from ' ./perguntas.js';
+import { aleatorio, nome } from './aleatorio.js';
+import { perguntas } from './perguntas.js';
+
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNOvamente = document.querySelector (".novamente-btn");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
 
-let atual = 0; 
+
+let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
 botaoIniciar.addEventListener('click', iniciaJogo);
 
-function iniciaJogo() {
+function iniciaJogo(){
     atual = 0;
-    historiaFinal = " ";
+    historiaFinal = "";
     telaInicial.style.display = 'none';
     caixaPerguntas.classList.remove("mostrar");
-    caixaAlternativas.classList.remove("mostar");
-    caixaResultado.classList.remove("mostar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
     mostraPergunta();
 }
-    
+
 function mostraPergunta() {
     if(atual >= perguntas.length){
         mostraResultado();
@@ -47,22 +49,35 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = aleatorio (opcaoSelecionada.afirmacao);
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, &{nome}`;
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = ""; 
+    caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar");
+
+    botaoJogarNovamente.addEventListener("click", jogarNovamente);
+
 }
 
-function substituiNome() {
-    for(const pergunta of perguntas) {
-        pergunta.enunciado = pergunta.enunciado.replace(/voce/g, nome);
+function jogarNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+
+    mostraPergunta();
+   
+}
+
+function substituiNome(){
+    for(const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
     }
-
 }
-SubstituiNome();
+
+substituiNome();
